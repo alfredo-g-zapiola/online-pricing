b1420cfbc00388bb28355b67bffeaa1020f3399c
a01023437 <agimenezzapiola@gmail.com>
2022-09-06 14:16:35 +0200

    clairvoyant

--- online_pricing/environment.py
+++ online_pricing/environment.py
@@ -8,7 +8,7 @@ import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
 from online_pricing.influence_function import InfluenceFunctor
-
+import itertools
 
 class EnvironmentBase:
     def __init__(self, n_products: int = 5, n_groups: int = 3, hyperparameters: dict[str, Any] | None = None) -> None:
@@ -52,17 +52,24 @@ class EnvironmentBase:
                 (96, 57.6),
             ],
         }
+        self.n_prices = len(self.prices_and_margins[list(self.prices_and_margins.keys())[0]])
+        self.product_id_map = {0: "echo_dot",
+                               1: "ring_chime",
+                               2: "ring_f",
+                               3: "ring_v",
+                               4: "echo_show"
+        }
 
         # function parameters (can also be  opened with a json)
         self.distributions_parameters: dict[str, Any] = {
-            "n_people_params": [700, 0, 0],  # we have more poor people than rich people
+            "n_people_params": [700, 500, 200],  # we have more poor people than rich people
             "dirichlet_params": [
                 np.asarray([15, 10, 6, 5, 4, 6]),
                 np.asarray([12, 9, 6, 4, 3, 4]),
                 np.asarray([5, 5, 9, 7, 7, 8]),
             ],
             # for the quantity chosen daily we have a ... distribution
-            "quantity_demanded_params": [1, 2, 3],
+            "quantity_demanded_params": [1, 1.2, 1.8],
             # product graph probabilities
             "product_graph": [self.product_matrix(size=(n_products, n_products), fully_connected=self.fully_connected)]
             * n_groups,  # end of product_graph matrices list
@@ -94,6 +101,10 @@ class EnvironmentBase:
 
         # initialise R session
         self._init_r()
+
+        # value the objective function
+        self.rewards = dict()
+        self.clairvoyant = {}
 
     @staticmethod
     def _init_r() -> None:
@@ -169,7 +180,8 @@ class EnvironmentBase:
             return float(robjects.r("d")[0])
         else:
             curve_f = robjects.r["{}".format(f_name)]
-            return float(curve_f(price)[0])
+            clipper = robjects.r["clipper.f"]
+            return float(clipper(curve_f(price)[0])[0])
 
     def get_direct_clients(self) -> dict[str, list[tuple[int, int]]]:
         """
@@ -235,16 +247,6 @@ class EnvironmentBase:
 
         return m
 
-    # static for now
-    @staticmethod
-    def compute_clairvoyant() -> float:
-        """
-        Finds the optimal super arm that maximises the utility gains
-        :return: optimal super arm
-        """
-        clairvoyant = 100.0
-        return clairvoyant
-
     def yield_first_secondaries(self) -> list[npt.NDArray[int]]:
         """
         Sends to the simulator the two best products to be the secondaries.
@@ -306,3 +308,65 @@ class EnvironmentBase:
             random_matrix = flatten_matrix.reshape(size)
 
         return random_matrix
+
+    def compute_clairvoyant(self):
+        """
+        For every price combination (so it is a carthesian product of the possible prices with itself)
+        , obtain the expected mean margin.
+        I.e. we compute a grid search working with expected values
+        Note it was tested:
+            if the margins are all 1, and there is 1 user per group then (0,0,0,0,0) is the best one (we maximise influence function),
+            and moreover its clairvoyant is below 1 (probability measure)
+        :return:
+        """
+        push_uncertain = self.uncertain_demand_curve  # save config
+        self.uncertain_demand_curve = False  # so we take the mean value
+        push_alpha_context = self.context_generation
+        self.context_generation = True
+        expected_alpha_r = self.yield_expected_alpha()
+
+        rewards = {}
+        maximum = 0.
+        max_arm = ""
+
+        # explore the carthesian product of the possible prices (5 values) with itself 5 times
+        for price_config in itertools.product(list(range(self.n_prices)), repeat=self.n_products):
+            print("Current price config: ", str(price_config))
+            cur_reward = 0.
+            price_and_margin = lambda p: self.prices_and_margins[self.product_id_map[p]][price_config[p]]
+            def c_rate(p):
+                """ Compute the conversion rate fixed with fixed group and prices.
+                This is why the function is redefined daily"""
+                return self.sample_demand_curve(group=g, prod_id=p,
+                                                 price=price_and_margin(p)[0])
+
+            for g in range(self.n_groups):  # for each group
+                quantity = self.distributions_parameters["quantity_demanded_params"][g]
+                g_reward = 0.
+                for p in range(self.n_products):  # for each starting product
+                    g_reward += expected_alpha_r[g][p+1] * (  # plus one since 0 corresponds to leaving the website
+
+                        c_rate(p) *
+                        price_and_margin(p)[1] * quantity +
+                        sum([self._influence_functor(p, p2, c_rate,
+                                                     self.distributions_parameters["product_graph"][g]) *
+                             c_rate(p2) * price_and_margin(p2)[1] * quantity
+                             for p2 in range(self.n_products) if p2 != p])
+                    )
+                cur_reward += g_reward * self.distributions_parameters["n_people_params"][g]  # weight it according to number of people
+            cur_reward /= sum([self.distributions_parameters["n_people_params"][g]
+                               for g in range(self.n_groups)])  # normalise
+
+            rewards[str(price_config)] = cur_reward
+            if cur_reward > maximum:
+                maximum = cur_reward
+                max_arm = str(price_config)
+
+        # reset data members
+        self.uncertain_demand_curve = push_uncertain
+        self.context_generation = push_alpha_context
+
+        self.rewards = rewards
+        self.clairvoyant = {max_arm: maximum}
+
+        return rewards, max_arm
