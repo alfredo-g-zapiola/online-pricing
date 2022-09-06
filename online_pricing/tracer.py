bf45a15c46d775879825147508b909730b46291c
Filippo Manzardo <film@bendingspoons.com>
2022-09-06 09:12:53 +0200

    feat: add plotting of moving average

--- online_pricing/tracer.py
+++ online_pricing/tracer.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Tracer:
@@ -11,5 +12,30 @@ class Tracer:
         self.measurements.append(measurement)
 
     def plot(self) -> None:
-        plt.plot(self.measurements)
+        moving_average = self.moving_average()
+        plt.figure()
+        plt.plot(
+            self.measurements,
+            label="Mean Average Reward",
+            color="blue",
+            linewidth=0.75,
+        )
+        plt.plot(
+            moving_average,
+            label="Moving Average",
+            color="red",
+        )
+        plt.xlabel("Days")
+        plt.ylabel("Average Reward")
+        plt.legend()
         plt.show()
+
+    def moving_average(self) -> list[float]:
+        window = 5
+        average = []
+        for idx in range(len(self.measurements) - window + 1):
+            average.append(np.mean(self.measurements[idx : idx + window]))
+        for idx in range(window - 1):
+            average.insert(idx, np.mean(self.measurements[0 : 0 + idx]))
+
+        return average
